9faa621f3b79c68a80f467062427c1e6c2ce3832
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-01-11 19:42:23 +0900

    feat: naver analytics(gatsby Script component)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,41 +1,45 @@
 import React from "react";
-import { Link, PageProps } from "gatsby";
+import { Link, PageProps, Script } from "gatsby";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="relative">
-      <header className="text-white absolute top-0 left-0 w-full z-50">
-        <div className="flex items-center h-24 container mx-auto">
-          {/* Logo */}
-          <div className="flex items-center gap-2">
-            <img
-              className="rounded-full w-12 h-12 border-2 border-white"
-              src="/img/logo.jpg"
-              alt="Bisue"
-            />
-            <span className="text-2xl font-black">BISUE</span>
-          </div>
-          {/* Links */}
-          <div className="flex items-center ml-auto gap-2">
-            <a href="https://bisue.github.io" target="_blank">
+    <>
+      <Script src="//wcs.naver.net/wcslog.js"></Script>
+      <Script>{`if(!wcs_add) var wcs_add = {}; wcs_add["wa"] = "32b593592d0696"; if(window.wcs) { wcs_do(); }`}</Script>
+      <div className="relative">
+        <header className="text-white absolute top-0 left-0 w-full z-50">
+          <div className="flex items-center h-24 container mx-auto">
+            {/* Logo */}
+            <div className="flex items-center gap-2">
               <img
-                className="rounded-full w-8 h-8"
-                src="/img/web.svg"
-                alt="Blog"
+                className="rounded-full w-12 h-12 border-2 border-white"
+                src="/img/logo.jpg"
+                alt="Bisue"
               />
-            </a>
-            <a href="https://github.com/Bisue" target="_blank">
-              <img
-                className="rounded-full w-8 h-8"
-                src="/img/github.svg"
-                alt="Github"
-              />
-            </a>
+              <span className="text-2xl font-black">BISUE</span>
+            </div>
+            {/* Links */}
+            <div className="flex items-center ml-auto gap-2">
+              <a href="https://bisue.github.io" target="_blank">
+                <img
+                  className="rounded-full w-8 h-8"
+                  src="/img/web.svg"
+                  alt="Blog"
+                />
+              </a>
+              <a href="https://github.com/Bisue" target="_blank">
+                <img
+                  className="rounded-full w-8 h-8"
+                  src="/img/github.svg"
+                  alt="Github"
+                />
+              </a>
+            </div>
           </div>
-        </div>
-      </header>
-      <main>{children}</main>
-    </div>
+        </header>
+        <main>{children}</main>
+      </div>
+    </>
   );
 };
 
