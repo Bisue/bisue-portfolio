32dbd6f60b4c3bcb3a5a3adf2c70eb3d02c36334
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-01-12 14:37:55 +0900

    feat: add interactive to InternalLink

--- src/components/InternalLink.tsx
+++ src/components/InternalLink.tsx
@@ -12,15 +12,32 @@ interface InternalLinkProps {
 const InternalLink: React.FC<InternalLinkProps> = ({ link }) => {
   return (
     <AnchorLink
-      className="flex items-center gap-3 tracking-wider"
+      className="flex items-center gap-3 tracking-wider group"
       to={link.target}
     >
-      <img src="/img/internal.svg" alt="내부 링크" />
+      <div className="relative">
+        <img
+          className="transition opacity-75 group-hover:translate-y-2 group-hover:opacity-100"
+          src="/img/internal.svg"
+          alt="내부 링크"
+        />
+        <img
+          className="absolute top-0 left-0 opacity-0 transition group-hover:-translate-y-2 group-hover:opacity-25"
+          src="/img/internal.svg"
+          alt="내부 링크"
+        />
+      </div>
       <div>
-        <div className="font-bold" style={{ fontSize: 24 }}>
+        <div
+          className="font-bold text-white/85 transition group-hover:text-white/100"
+          style={{ fontSize: 24 }}
+        >
           {link.title}
         </div>
-        <div className="text-white text-opacity-75" style={{ fontSize: 16 }}>
+        <div
+          className="text-white/50 transition group-hover:text-white-75"
+          style={{ fontSize: 16 }}
+        >
           {link.description}
         </div>
       </div>
