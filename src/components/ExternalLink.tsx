a7b62c6abb86998625e846a93149d0c28114c585
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-01-12 14:45:42 +0900

    feat: add interactivity to ExternalLink

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -11,16 +11,26 @@ interface ExternalLinkProps {
 const ExternalLink: React.FC<ExternalLinkProps> = ({ link }) => {
   return (
     <a
-      className="flex items-center gap-3 tracking-wider"
+      className="flex items-center gap-3 tracking-wider group"
       href={link.href}
       target="_blank"
     >
-      <img src="/img/external.svg" alt="외부 링크" />
+      <img
+        className="opacity-75 transition group-hover:translate-x-1 group-hover:opacity-100"
+        src="/img/external.svg"
+        alt="외부 링크"
+      />
       <div>
-        <div className="font-bold" style={{ fontSize: 24 }}>
+        <div
+          className="font-bold text-white/85 transition group-hover:text-white/100"
+          style={{ fontSize: 24 }}
+        >
           {link.title}
         </div>
-        <div className="text-white text-opacity-75" style={{ fontSize: 16 }}>
+        <div
+          className="text-white/50 transition group-hover:text-white-75"
+          style={{ fontSize: 16 }}
+        >
           {link.description}
         </div>
       </div>
